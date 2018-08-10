975b22dc1f93d4c0a5a5b43e1573840c7314b27a
Warrake Hussein <warrakehussein@gmail.com>
2018-08-10 13:16:50 +0100

    fixed remote mongoDB connection

--- schemas/users.js
+++ schemas/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://twitpadi:jakande50@ds131971.mlab.com:31971/twitpadi');
+mongoose.connect('mongodb://citadel:jakande@50@ds151202.mlab.com:51202/heroku_3wpz3631');
+//mongoose.connect('mongodb://twitpadi:jakande50@ds131971.mlab.com:31971/twitpadi');
+
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 const UserSchema = new Schema({
